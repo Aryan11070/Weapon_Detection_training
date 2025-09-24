fe7ce8a55f4d29b948658210b4ca60a9c7bcdedd
Aryan11070 <aryankota1976@gmail.com>
2025-09-25 00:35:35 +0530

    Update generate_train.py

--- generate_train.py
+++ generate_train.py
@@ -1,13 +1,9 @@
 import os
 
 image_files = []
-os.chdir(os.path.join("train", "images"))   # go into train/images
-
-for filename in os.listdir(os.getcwd()):
+for filename in os.listdir("train/images"):
     if filename.endswith(".jpg") or filename.endswith(".png"):
         image_files.append("train/images/" + filename)
-
-os.chdir("../..")   # go back to dataset root
 
 with open("train.txt", "w") as outfile:
     for image in image_files:
